4a0d670151c59de669b8c15eaaeef34e5bdbd529
Artme <artalebar@gmail.com>
2020-11-15 17:31:08 +0300

    implemented correct word label update

--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -130,10 +130,24 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    func updateCorrectWordLabel() {
+        var displayWord = [String]()
+        for letter in  currentGame.guessedWord {
+            displayWord.append(String(letter))
+        }
+        correctWordLabel.text = displayWord.joined(separator: "  ")
+    }
+    
     func updateUI() {
         let movesRemaining  = currentGame.incorrectMovesRemaining
-        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+        
+        //пример тернарного оператора с множеством условий
+        //let imageNumber = movesRemaining < 0 ? 0 : movesRemaining < 8 ? movesRemaining : 7
+        
+        let imageNumber = (movesRemaining + 64) % 8
+        let image = "Tree\(imageNumber)"
         treeImageView.image = UIImage(named: image)
+        updateCorrectWordLabel()
         scoreLabel.text = "Выигрыши: \(totalWins), проигрыши: \(totalLosses)"
     }
     
@@ -145,6 +159,9 @@ class ViewController: UIViewController {
     // MARK: - IB Actions
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letter = sender.title(for: .normal)!
+        currentGame.playerGuessed(letter: Character(letter))
+        updateUI()
     }
     
     
